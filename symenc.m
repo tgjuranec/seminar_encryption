c6a45c4e442e5a2d2319f9e27d087e90f54f403f
Tomislav <tgjuranec@gmail.com>
2022-06-02 15:31:46 +0200

    Modified symmetric encryption script
    Added bash script commented

--- symenc.m
+++ symenc.m
@@ -5,17 +5,28 @@ save -binary sym_image.raw i
 
 
 ##BASH SCRIPT
-###command removes first 54!!!! bytes from the image.raw
-##tail -c +55 image.raw > image_nohead.raw
+
+###key=A095A2EB2459A004B0C8D473817E6295E834B4AF272A111B6BE34982BB347320
+###iv =CFD734DBE2C9F0253065E8CBA25CF1DA
 ##
-###encryption with public key
-##enc -k ssl/fedora_posao/public.pem -e image_nohead.raw image_nohead.enc
 ##
-###encrypted image payload has increased size -> reduce size to first 192k
-##head -c 196608 image_nohead.enc > image_short.enc
+#####command removes first 54!!!! bytes from the image.raw                      
 ##
-###add header to image.enc
-##head -c 54 image.raw | cat - image_short.enc > image.enc
+##tail -c +55 sym_image.raw > sym_image_nohead.raw
+##
+##
+##openssl enc -aes-256-ecb -nopad -nosalt -e \
+##        -in sym_image_nohead.raw -out sym_image.enc \
+##        -K 'A095A2EB2459A004B0C8D473817E6295E834B4AF272A111B6BE34982BB347320'
+##
+#####add header to final file: sym_enc.bmp 
+##
+##head -c 54 sym_image.raw | cat - sym_image.enc > sym_enc.bmp
+##
+###removing temporary files
+##
+##rm sym_image.raw sym_image_nohead.raw sym_image.enc 
+
 
 system('bash ./symenc.sh')
 
